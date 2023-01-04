fcdd617d0f33333626a802c3e5f0cade5569d006
Harsh Singh <alexlin2922@gmail.com>
2023-01-04 23:54:02 +0530

    navigation issue fixed

--- app.js
+++ app.js
@@ -3,20 +3,20 @@
 
 function close_StartingInput(){
     var StartingInput = document.getElementById("starting_inputs");
-    StartingInput.style.transform="translateX(100%)";
+    StartingInput.style.transform="translateX(10000px)";
 }
 function open_StartingInput(){
     var StartingInput = document.getElementById("starting_inputs");
-    StartingInput.style.transform="translateX(0%)";
+    StartingInput.style.transform="translateX(0px)";
 }
 // instructions navigation
 
 
 function close_instructions(){
     var StartingInput = document.getElementById("instructions");
-    StartingInput.style.transform="translateX(100%)";
+    StartingInput.style.transform="translateX(10000px)";
 }
 function open_instructions(){
     var StartingInput = document.getElementById("instructions");
-    StartingInput.style.transform="translateX(0%)";
+    StartingInput.style.transform="translateX(0px)";
 }
