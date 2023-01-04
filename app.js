0da974f20fe5a24597e5e40348a398d1df45783b
Harsh Singh <alexlin2922@gmail.com>
2023-01-04 22:37:11 +0530

    first draft

---
+++ app.js
@@ -0,0 +1,22 @@
+// starting_inputs navigation
+
+
+function close_StartingInput(){
+    var StartingInput = document.getElementById("starting_inputs");
+    StartingInput.style.transform="translateX(100%)";
+}
+function open_StartingInput(){
+    var StartingInput = document.getElementById("starting_inputs");
+    StartingInput.style.transform="translateX(0%)";
+}
+// instructions navigation
+
+
+function close_instructions(){
+    var StartingInput = document.getElementById("instructions");
+    StartingInput.style.transform="translateX(100%)";
+}
+function open_instructions(){
+    var StartingInput = document.getElementById("instructions");
+    StartingInput.style.transform="translateX(0%)";
+}
