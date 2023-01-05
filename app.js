ac896ddf4336bcafa7937d2d5d5ae67b88f07339
Harsh Singh <alexlin2922@gmail.com>
2023-01-05 12:15:56 +0530

    first input working

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+var text_box = document.getElementById("time_count");
+var user_name_input = document.getElementById("UserName");
+var player_name_span = document.getElementById("player_name_span");
+var round_max_input = document.getElementById("rounds");
+var round_max_span = document.getElementById("round_max");
+
+
+
+
+
+
+
+////directly executing code
+user_name_input.addEventListener("input", function() {
+    player_name_span.innerHTML =user_name_input.value;
+  });
+round_max_input.addEventListener("input", function() {
+    round_max_span.innerHTML =round_max_input.value;
+  });
+
+
+
+
+
+
+
+
 // starting_inputs navigation
 
 
@@ -19,4 +46,49 @@ function close_instructions(){
 function open_instructions(){
     var StartingInput = document.getElementById("instructions");
     StartingInput.style.transform="translateX(0px)";
+}
+// main_screen navigation
+
+
+function close_main(){
+    var StartingInput = document.getElementById("main");
+    StartingInput.style.transform="translateX(10000px)";
+}
+function open_main(){
+    var StartingInput = document.getElementById("main");
+    StartingInput.style.transform="translateX(0px)";
+}
+
+
+
+// functions for text pop ups
+
+
+function rock_text(){
+    text_box.innerHTML = "ROCK";   
+}
+function paper_text(){
+    text_box.style.transform="scale(1.5)";
+    text_box.innerHTML = "PAPER";   
+}
+function scissor_text(){
+    text_box.style.transform="scale(2)";
+    text_box.innerHTML = "SCISSORS!";   
+}
+function clear_text(){
+    text_box.style.transform="scale(1)";
+    text_box.innerHTML = "";  
+}
+function bot_won_text(){
+    text_box.innerHTML = "BOT Wins this round";  
+}
+function player_won_text(){
+    text_box.innerHTML = " you won this round";  
+}
+
+function RPS_text(){
+    rock_text();
+    setTimeout(paper_text, 500);
+    setTimeout(scissor_text, 1000);
+    setTimeout(clear_text, 1500)
 }
